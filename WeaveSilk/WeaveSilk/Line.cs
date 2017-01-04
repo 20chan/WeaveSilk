2779292644a1bffe5fce19c85bfb5e34ed8040cb
philly <sonagi.philly@gmail.com>
2017-01-04 16:47:30 +0900

    Add Trail class

---
+++ WeaveSilk/WeaveSilk/Line.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WeaveSilk
+{
+    public static class Line
+    {
+        private static Texture2D t;
+        public static void DrawLine(GraphicsDevice device, SpriteBatch sb, Vector2 v1, Vector2 v2, int bold = 1) 
+            => DrawLine(device, sb, v1, v2, Color.Black, bold);
+        public static void DrawLine(GraphicsDevice device, SpriteBatch sb, Vector2 v1, Vector2 v2, Color color, int bold = 1)
+        {
+            if (t == null)
+            {
+                t = new Texture2D(device, 1, 1);
+                t.SetData<Color>(
+                    new Color[] { color });// fill the texture with white
+            }
+            Vector2 edge = v2 - v1;
+            // calculate angle to rotate line
+            float angle =
+                (float)Math.Atan2(edge.Y, edge.X);
+
+
+            sb.Draw(t,
+                new Rectangle(// rectangle defines shape of line and position of start of line
+                    (int)v1.X,
+                    (int)v1.Y,
+                    (int)edge.Length(), //sb will strech the texture to fill this rectangle
+                    bold), //width of line, change this to make thicker line
+                null,
+                color, //colour of line
+                angle,     //angle of line (calulated above)
+                new Vector2(0, 0), // point in line about which to rotate
+                SpriteEffects.None,
+                0);
+        }
+    }
+}
---
+++ WeaveSilk/WeaveSilk/Trail.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WeaveSilk
+{
+    public class Trail : DrawableGameComponent
+    {
+        public float LifeTime { get; set; } = 60;
+        private List<Component> _points;
+        private SpriteBatch _spriteBatch;
+
+        public Trail(Game game, SpriteBatch sb) : base(game)
+        {
+            _spriteBatch = sb;
+            _points = new List<Component>();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            var state = Mouse.GetState();
+
+            if (_points.Count > 0 && _points.First().IsDead)
+                _points.RemoveAt(0);
+
+            _points.Add(new Component(this, state.X, state.Y));
+
+            _points.ForEach(c => c.Update());
+
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            for(int i = 0; i < _points.Count - 1; i++)
+            {
+                Line.DrawLine(Game.GraphicsDevice, _spriteBatch, _points[i].Location, _points[i + 1].Location, 3);
+            }
+            base.Draw(gameTime);
+        }
+
+        private class Component
+        {
+            public float X { get; set; }
+            public float Y { get; set; }
+            public Vector2 Location => new Vector2(X, Y);
+            public bool IsDead => _current > _trail.LifeTime;
+
+            private Trail _trail;
+            private float _current;
+
+            public Component(Trail trail, float x = 0, float y = 0)
+            {
+                _trail = trail;
+                X = x;
+                Y = y;
+            }
+
+            public void Update() => _current++;
+        }
+    }
+}
